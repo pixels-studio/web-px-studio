8c223ee8ed169151a42e17a9c1fdb427342d8b4a
abhi <abhishek.kambli@samespace.com>
2024-08-20 09:28:47 +0530

    added id to projects array

--- src/utility/constants.js
+++ src/utility/constants.js
@@ -10,46 +10,55 @@ import ProjectTeaser9 from "@/assets/projects/project-teaser-9.png";
 
 export const PROJECTS = [
   {
+    id: 1,
     image: ProjectTeaser1,
     title: "Unsplash Desktop App",
     subtitle: "Native MacOS Client for Unsplash",
   },
   {
+    id: 2,
     image: ProjectTeaser2,
     title: "Unsplash Figma Plugin",
     subtitle: "Figma Extension for designers",
   },
   {
+    id: 3,
     image: ProjectTeaser3,
     title: "Unsplash for iOS",
     subtitle: "Redesigned for intuitive mobile experience and visual impact.",
   },
   {
+    id: 4,
     image: ProjectTeaser4,
     title: "Interview Screen",
     subtitle: "AI Powered Candidate screening",
   },
   {
+    id: 5,
     image: ProjectTeaser5,
     title: "Interview Screen",
     subtitle: "AI Powered Candidate screening",
   },
   {
+    id: 6,
     image: ProjectTeaser6,
     title: "Interview Screen",
     subtitle: "AI Powered Candidate screening",
   },
   {
+    id: 7,
     image: ProjectTeaser7,
     title: "Interview Screen",
     subtitle: "AI Powered Candidate screening",
   },
   {
+    id: 8,
     image: ProjectTeaser8,
     title: "Interview Screen",
     subtitle: "AI Powered Candidate screening",
   },
   {
+    id: 9,
     image: ProjectTeaser9,
     title: "Interview Screen",
     subtitle: "AI Powered Candidate screening",
